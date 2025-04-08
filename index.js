e7a17309f94a2b0e3eefc50a66ca13d28073c963
shaliyur <shaliyur@gmail.com>
2025-04-08 12:55:54 -0400

    added form element in javascript and associated event listener

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 console.log('My code is running');
-console.log(document.querySelector('h1'));
-
-let test_header = document.createElement('h2');
-test_header.innerHTML = 'This is a test';
+//console.log(document.querySelector('h1'));
 
 
+
+let btn = document.querySelector('button');
+let user_input = document.querySelector('input');
+let form = document.querySelector('form');
+
+function createTask(eventObject){
+  //This is how we access user input - eventObject.target.value
+  let new_task = document.createElement('div');
+  let task_text = eventObject.target.value;
+  new_task.appendChild(task_text);
+  let container = document.querySelector('#container');
+  container.appendChild(new_task);
+  container.insertBefore(new_task, container.childNodes[0]);
+}
+
+form.addEventListener("submit", function(eventObject){
+  createTask(eventObject);
+});
 
 
